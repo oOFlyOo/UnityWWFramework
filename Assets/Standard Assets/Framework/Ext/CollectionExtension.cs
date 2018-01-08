973f38a7fe9b000d502c95b340aa605470e14c2c
Fly <393830887@qq.com>
2018-01-08 19:45:31 +0800

    增加对 Collection GC 优化

---
+++ Assets/Standard Assets/Framework/Ext/CollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WWFramework.Extension
+{
+    public static class CollectionExtension
+    {
+        [Obsolete("Unity 5.6 开始不再有 GC")]
+        public static void Foreach<TKey>(this List<TKey> list, Action<TKey> callback)
+        {
+            var count = list.Count;
+            for (int i = 0; i < count; i++)
+            {
+                callback(list[i]);
+            }
+        }
+
+
+        [Obsolete("Unity 5.6 开始不再有 GC")]
+        public static void Foreach<TKey, TValue>(this Dictionary<TKey, TValue> dict, Action<TKey, TValue> callback)
+        {
+            if (dict != null)
+            {
+                var enumerator = dict.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    callback(enumerator.Current.Key, enumerator.Current.Value);
+                }
+            }
+        }
+    }
+}

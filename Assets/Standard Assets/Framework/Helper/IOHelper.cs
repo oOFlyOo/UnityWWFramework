53854c8d9c5c9797d959793889122e228cd7fb1e
Fly <393830887@qq.com>
2018-01-11 16:18:19 +0800

    Add IOHelper

---
+++ Assets/Standard Assets/Framework/Helper/IOHelper.cs
@@ -0,0 +1,51 @@
+﻿
+using System.IO;
+
+namespace WWFramework.Helper
+{
+    public static class IOHelper
+    {
+        public static bool IsDirectory(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+
+        public static bool DeleteFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool CreateDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public static bool DeleteDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

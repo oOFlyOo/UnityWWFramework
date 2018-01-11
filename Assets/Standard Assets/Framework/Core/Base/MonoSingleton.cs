54128303e37c70cac568f5e7eaa974ce5a1a2eea
Fly <393830887@qq.com>
2018-01-11 17:44:52 +0800

    Add 单例

---
+++ Assets/Standard Assets/Framework/Core/Base/MonoSingleton.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+
+namespace WWFramework.Core
+{
+    public abstract class MonoSingleton<T>: MonoBehaviour where T:MonoSingleton<T>
+    {
+        #region 单例
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var go = new GameObject(typeof(T).Name);
+                    DontDestroyOnLoad(go);
+                    _instance = go.AddComponent<T>();
+                    _instance.OnInit();
+                }
+
+                return _instance;
+            }
+        }
+
+
+        public static bool DisposeInstance()
+        {
+            if (_instance != null)
+            {
+                _instance.OnDispose();
+                Destroy(_instance.gameObject);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region 辅助
+
+        protected virtual void OnInit()
+        {
+        }
+
+        protected virtual void OnDispose()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Assets/Standard Assets/Framework/Core/Base/Singleton.cs
@@ -0,0 +1,56 @@
+﻿
+namespace WWFramework.Core
+{
+    public abstract class Singleton<T> where T : Singleton<T>, new()
+    {
+        #region 单例
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                    _instance.OnInit();
+                }
+
+                return _instance;
+            }
+        }
+
+
+        protected Singleton()
+        {
+
+        }
+
+        public static bool DisposeInstance()
+        {
+            if (_instance != null)
+            {
+                _instance.OnDispose();
+                _instance = null;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region 辅助
+
+        protected virtual void OnInit()
+        {
+        
+        }
+
+        protected virtual void OnDispose()
+        {
+        
+        }
+        #endregion
+    }
+}

d414747d7e63a81efba35b1c2fa91fedf0f653d0
Fly <393830887@qq.com>
2018-01-12 18:23:33 +0800

    Add UnityEngineExtension

---
+++ Assets/Standard Assets/Framework/Ext/UnityEngineExtension.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+namespace WWFramework.Extension
+{
+    public static class UnityEngineExtension
+    {
+        public static T GetMissingComponent<T>(this GameObject go) where T : Component
+        {
+            var com = go.GetComponent<T>();
+            if (com == null)
+            {
+                com = go.AddComponent<T>();
+            }
+
+            return com;
+        }
+    }
+}

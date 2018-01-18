01ccd8c0718e6fae47599cd76e97727ff29180d3
Fly <393830887@qq.com>
2018-01-18 11:10:06 +0800

    Add 提交FPS
    Development目录用于常规开发测试使用

---
+++ Assets/Standard Assets/Framework/Development/FPS.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+
+namespace WWFramework.Development
+{
+    public class FPS : MonoBehaviour
+    {
+        [SerializeField]
+        private bool _forceCloseVSync = true;
+        [SerializeField, Range(1, 3)]
+        private float _checkDelta = 1f;
+
+        private int _frameCount;
+        private float _timePassed;
+
+        private float _fps;
+        private GUIStyle _style;
+
+        private void Start()
+        {
+            if (_forceCloseVSync)
+            {
+                QualitySettings.vSyncCount = 0;
+                Application.targetFrameRate = -1;
+            }
+
+            _style = new GUIStyle();
+            _style.normal.background = null;
+            _style.normal.textColor = Color.yellow;
+        }
+
+        private void Update()
+        {
+            _frameCount++;
+            _timePassed += Time.deltaTime;
+
+            if (_timePassed > _checkDelta)
+            {
+                _fps = _frameCount / _timePassed;
+
+                _frameCount = 0;
+                _timePassed = 0f;
+            }
+        }
+
+        private void OnGUI()
+        {
+            var fontSize = Screen.height/20;
+            _style.fontSize = fontSize;
+
+            GUI.Label(new Rect(0, 0, fontSize * 3, fontSize), string.Format("{0:F0}", _fps), _style);
+        }
+    }
+}

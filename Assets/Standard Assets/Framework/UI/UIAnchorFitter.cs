0816c4d2b6e1850ff0ce468a44e81516abeb9043
赵佩豪 <peihao.zhao@centurygame.com>
2020-08-05 21:37:19 +0800

    UI辅助

---
+++ Assets/Standard Assets/Framework/UI/UIAnchorFitter.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace WWFramework.Uitl.UI
+{
+    public class UIAnchorFitter : UIBehaviour, ILayoutSelfController
+    {
+        private enum Side
+        {
+            Left,
+            Right,
+            Top,
+            Bottom,
+        }
+        
+        [SerializeField]
+        private RectTransform _anchorLeft;
+        [SerializeField]
+        private Side _leftSide = Side.Right;
+        
+        [SerializeField]
+        private RectTransform _anchorRight;
+        [SerializeField]
+        private Side _rightSide = Side.Left;
+        
+        [SerializeField]
+        private RectTransform _anchorTop;
+        [SerializeField]
+        private Side _topSide = Side.Bottom;
+        
+        [SerializeField]
+        private RectTransform _anchorBottom;
+        [SerializeField]
+        private Side _bottomSize = Side.Top;
+
+        private RectTransform _cacheRect;
+        private RectTransform rectTransform
+        {
+            get
+            {
+                if (!_cacheRect)
+                {
+                    _cacheRect = GetComponent<RectTransform>();
+                }
+
+                return _cacheRect;
+            }
+        }
+        
+        private Vector3[] _tempCorners = new Vector3[4];
+        
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            SetDirty();
+        }
+    
+        public void SetLayoutHorizontal()
+        {
+            UpdateAnchors();
+        }
+
+        public void SetLayoutVertical()
+        {
+            UpdateAnchors();
+        }
+
+        [ContextMenu("UpdateAnchors")]
+        public void UpdateAnchors()
+        {
+            _anchorLeft.GetWorldCorners(_tempCorners);
+            var right = _tempCorners[2];
+            ((RectTransform) rectTransform.parent).GetWorldCorners(_tempCorners);
+            var parentLeft = _tempCorners[0];
+            rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, right.x - parentLeft.x, 160);
+            // Vector2 leftPoint;
+            // RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)rectTransform.parent, left, null, out leftPoint);
+            // rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, );
+        }
+        
+        protected void SetDirty()
+        {
+            if (!IsActive())
+                return;
+
+            LayoutRebuilder.MarkLayoutForRebuild(rectTransform);
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            SetDirty();
+        }
+
+#endif
+    }
+}
---
+++ Assets/Standard Assets/Framework/UI/UICollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace WWFramework.Uitl.UI
+{
+    public class UICollider : Graphic, ICanvasRaycastFilter
+    {
+        protected override void Awake() {
+            base.Awake();
+        
+            color = Color.clear;
+        }
+    
+        /// <summary>
+        /// 这个非必要，如果需要填充颜色的话；不过需要填充颜色的话Image可能更合适
+        /// </summary>
+        /// <param name="toFill"></param>
+        protected override void OnPopulateMesh(VertexHelper toFill) {
+            toFill.Clear();
+        }
+    
+        public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+        {
+            return true;
+        }
+    }
+}

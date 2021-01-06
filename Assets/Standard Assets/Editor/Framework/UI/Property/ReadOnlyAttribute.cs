99f3ce7a2684a910bb70f756971fd6905cd4ac13
赵佩豪 <peihao.zhao@centurygame.com>
2021-01-06 17:14:51 +0800

    ReadOnly

---
+++ Assets/Standard Assets/Editor/Framework/UI/Property/ReadOnlyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WWFramework.UI.Editor
+{
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+}

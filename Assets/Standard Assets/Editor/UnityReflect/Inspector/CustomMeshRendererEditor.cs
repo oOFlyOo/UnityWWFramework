1db1dd32b01ff602b10dc52daf611bdd149b932a
赵佩豪 <peihao.zhao@centurygame.com>
2021-01-15 17:16:24 +0800

    另类的反射方式，需要什么接口或者类，继承或者封装来处理

---
+++ Assets/Standard Assets/Editor/UnityReflect/Inspector/CustomMeshRendererEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace WWFramework.UnityReflect.Inspector.Editor
+{
+    [CustomEditor(typeof(MeshRenderer))]
+    internal class CustomMeshRendererEditor : MeshRendererEditor
+    {
+        private MeshRenderer _renderer;
+        private int _sortingOrder;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+        
+            _renderer = serializedObject.targetObject as MeshRenderer;
+            _sortingOrder = _renderer.sortingOrder;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            _sortingOrder = EditorGUILayout.IntField("SortingOrder", _sortingOrder);
+            _renderer.sortingOrder = _sortingOrder;
+        }
+    }
+}

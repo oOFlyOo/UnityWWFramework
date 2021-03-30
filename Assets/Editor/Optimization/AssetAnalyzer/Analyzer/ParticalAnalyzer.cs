7f404e33a1b3bdcb482a90fd40f27537d2d019b2
赵佩豪 <peihao.zhao@centurygame.com>
2021-03-30 14:41:09 +0800

    优化Analyzer

--- Assets/Editor/Optimization/AssetAnalyzer/Analyzer/ParticalAnalyzer.cs
+++ Assets/Editor/Optimization/AssetAnalyzer/Analyzer/ParticalAnalyzer.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using WWFramework.Helper.Editor;
@@ -9,29 +10,56 @@ namespace WWFramework.Optimization.Editor
 {
     public class ParticalAnalyzer:BaseAssetAnalyzer<GameObject>
     {
-        public override void Analyse(Object[] assets)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void CorrectAll()
-        {
-            throw new System.NotImplementedException();
-        }
-
         protected override List<GameObject> GetFilterObjects(Object[] assets)
         {
-            throw new System.NotImplementedException();
+            return assets.ToList().ConvertAll(input => input as GameObject);
         }
 
         protected override List<GameObject> GetProjectObjects()
         {
-            throw new System.NotImplementedException();
+            return EditorAssetHelper.FindAssetsPaths(EditorAssetHelper.SearchFilter.Prefab)
+                .ConvertAll(input => AssetDatabase.LoadAssetAtPath<GameObject>(input));
         }
 
         protected override bool IsSickAsset(GameObject obj, bool needCorrect = false, bool needSave = true)
         {
-            throw new System.NotImplementedException();
+            var correct = false;
+
+            var particles = obj.GetComponentsInChildren<ParticleSystemRenderer>(true);
+            if (particles.Length > 0)
+            {
+                foreach (var particle in particles)
+                {
+                    if (particle.renderMode != ParticleSystemRenderMode.Mesh && particle.mesh != null)
+                    {
+                        if (needCorrect)
+                        {
+                            correct = true;
+                            particle.mesh = null;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            if (correct)
+            {
+                EditorUtility.SetDirty(obj);
+                // AssetDatabase.ForceReserializeAssets(new []
+                // {
+                //     AssetDatabase.GetAssetPath(obj),
+                // });
+                
+                if (needSave)
+                {
+                    AssetDatabase.SaveAssets();
+                }
+            }
+
+            return false;
         }
     }
 }
